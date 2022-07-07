62c60fc6145774d23512e922dad936815d9263a9
NHSanto <nahid170053@diit.edu.bd>
2022-07-07 21:32:18 +0600

    all added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 
 import Navbar from "./Components/Navbar";
-import DoctorSection from "./Components/DoctorSection";
 import SignUpForm from "./Components/SignUpForm";
 
 function App() {
